cd946e4ffe276b5f3d95e7f52ab2cef70edf564d
Enrique Gonzalez <egonzalezh94@gmail.com>
2017-10-08 19:47:17 -0700

    0.1.0 Release (#12)
    * Include setup file and prepare everything to upload to PyPI
    
    * Add coverage file
    
    * Add pypandoc to turn markdown to restructured text for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 
-with open('README.md') as f:
-    readme = f.read()
-
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 setup(
     name = 'testobject',
     packages = ['testobject'],
@@ -13,7 +15,7 @@ setup(
     license='MIT',
     url = 'https://github.com/enriquegh/testobject-python-api',
     install_requires=['requests', 'pyyaml'],
-    long_description=readme,
+    long_description=long_description,
     keywords = ['wrapper-api', 'testobject'],
     classifiers = [],
 )
