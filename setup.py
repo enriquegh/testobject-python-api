9c35f304cd13c7551f982479d64cf03043300dc1
Enrique Gonzalez <egonzalezh94@gmail.com>
2019-01-26 16:09:56 -0800

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 setup(
     name = 'testobject',
     packages = ['testobject'],
-    version = '0.2.0',
+    version = '0.3.0',
     description = 'Python API wrapper for TestObject',
     author = 'Enrique Gonzalez',
     author_email = 'egonzalezh94@gmail.com',
