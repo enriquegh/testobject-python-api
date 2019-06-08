9171dcdd4e589259aba23d902b962b8adb41b26a
Enrique Gonzalez <egonzalezh94@gmail.com>
2019-06-07 18:19:31 -0700

    drop Python 3.4 support (#36)
    Requests dropped support and in general is a dead version

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author = 'Enrique Gonzalez',
     author_email = 'egonzalezh94@gmail.com',
     license='MIT',
+     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     url = 'https://github.com/enriquegh/testobject-python-api',
     install_requires=['requests', 'pyyaml'],
     long_description=long_description,
